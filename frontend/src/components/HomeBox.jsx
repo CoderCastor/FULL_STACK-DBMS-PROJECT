50f69b882c3e0fcb8016039e466c035cd0ae7767
codercastor <tejasshinde935@gmail.com>
2024-10-15 15:00:26 +0530

    notices implemented

--- frontend/src/components/HomeBox.jsx
+++ frontend/src/components/HomeBox.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { PiStudentBold } from "react-icons/pi";
 
-function HomeBox() {
+function HomeBox(data) {
   return (
-    <div className='bg-blue-200 w-4/5 lg:w-[25%] h-[100px] rounded-ee-3xl rounded-ss-3xl p-1 lg:h-[100px] flex overflow-hidden px-5'>
+    <div className='bg-blue-200 w-4/5 lg:w-[25%] h-[100px] rounded-ee-3xl rounded-ss-3xl p-1 lg:h-[100px] flex overflow-hidden px-5 shadow-xl'>
         <div className="logo  w-[35%] flex justify-center items-center">
-        <div className='bg-blue-500 w-[80px] h-[50px] flex justify-center items-center text-3xl rounded-ee-3xl rounded-ss-3xl'><PiStudentBold  /></div>
+        <div className='bg-blue-500 text-white w-[80px] h-[50px] flex justify-center items-center text-3xl rounded-ee-3xl rounded-ss-3xl'><PiStudentBold  /></div>
         </div>
         <div className="text w-[65%] flex flex-col items-center justify-between py-2">
             <div className="data text-3xl font-semibold mt-2">77</div>
--- frontend/src/pages/Admin/Home.jsx
+++ frontend/src/pages/Admin/Home.jsx
@@ -2,14 +2,48 @@ import React from 'react'
 import HomeBox from '../../components/HomeBox'
 
 function AdminDashBoard() {
+
+  const data = [
+    {
+      title:"Total Students"
+    },
+    {
+      title:"Total Teachers"
+    },
+    {
+      title:"Total Classes"
+    },
+    {
+      title:"Fees Collections"
+    }
+  ]
+
   return (
     <div className='w-full'>
       <div className='w-[100%] lg:w-3/5 mx-auto'>
         <div className='Boxes mt-2 lg:mt-10 flex flex-col lg:flex-row gap-3 lg:justify-evenly items-center py-4'>
-          <HomeBox/>
-          <HomeBox/>
-          <HomeBox/>
-          <HomeBox/>
+          <HomeBox data={data} />
+          <HomeBox data={data} />
+          <HomeBox data={data} />
+          <HomeBox data={data} />
+        </div>
+        <div className="notices mt-5 bg-purple-200 w-[90%] lg:w-full text-2xl mx-auto rounded-xl overflow-hidden">
+          <div className="heading bg-purple-400 shadow-2xl text-xl lg:text-4xl text-white h-10 px-3 lg:py-3 lg:h-auto lg:px-5 flex items-center">Notices</div>
+          <div className='px-3'>
+            notice 1
+          </div>
+          <hr className='bg-white' />
+          <div className='px-3'>
+            notice 2
+          </div>
+          <hr className='bg-white' />
+          <div className='px-3'>
+            notice 3
+          </div>
+          <hr className='bg-white' />
+          <div className='px-3'>
+            notice 4
+          </div>
         </div>
       </div>
     </div>
